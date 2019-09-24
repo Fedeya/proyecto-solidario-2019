6d0aa22344d1574ab67954007b4a38fd1dd25eee
Federico Minaya <elfedeminaya@gmail.com>
2019-09-24 08:49:06 -0300

    fixed bug no show list of contributors

--- app.js
+++ app.js
@@ -35,7 +35,66 @@ document.querySelector("#bici-p").addEventListener("click", () => {
   })
 })
 
+document.querySelector("#bici-ps").addEventListener("click", () => {
+
+  const p = [
+    "Ariel Aggio", "Agustin Almada",
+    "Avril Buffa", "Luciano Coronel", 
+    "Luciana Fernandez", "Matilda Garijo",
+    "Candela Gomez", "Ivan Ibarra",
+    "Luna Kies", "Nahuel Martinez",
+    "Federico Minaya", "Valentina Volpe",
+  ]
+
+  let participantes = "";
+
+  p.forEach(participante => {
+    participantes += `<li class="collection-item">${participante}</li>`
+  })
+
+  const wrapper = document.createElement("div");
+  wrapper.innerHTML = `
+    <ul class="collection">
+      ${participantes}
+    </ul>
+  `
+  console.log(wrapper.innerHTML);
+
+  swal({
+    content: wrapper
+  })
+})
+
 document.querySelector("#banco-p").addEventListener("click", () => {
+
+  const p = [
+    "Camila Esquivel", "Valentina Lloret",
+    "Martina Lopez", "Patricio Miranda",
+    "Valentina Navarro", "Pilar Octtinger",
+    "Selene Sierra", "Agustin Vega",
+    "Santino Volpe" 
+  ]
+
+  let participantes = "";
+
+  p.forEach(participante => {
+    participantes += `<li class="collection-item">${participante}</li>`
+  })
+
+  const wrapper = document.createElement("div");
+  wrapper.innerHTML = `
+    <ul class="collection">
+      ${participantes}
+    </ul>
+  `
+  console.log(wrapper.innerHTML);
+
+  swal({
+    content: wrapper
+  })
+})
+
+document.querySelector("#banco-ps").addEventListener("click", () => {
 
   const p = [
     "Camila Esquivel", "Valentina Lloret",
